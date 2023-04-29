ce2032c9626e49df466fb78eba8e376e834a8a16
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-04-29 15:31:33 -0700

    Working on variable,elements,and  functions layout

---
+++ app.js
@@ -0,0 +1,20 @@
+	/*----- constants -----*/
+    const PLAYERS = {
+		'0': 'white',
+		'1':'X-player',
+	   '-1': 'O-player'
+		
+	  };
+
+
+	/*----- state variables -----*/
+
+
+
+	/*----- cached elements  -----*/
+
+
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
