b7b0d069aded8a293ad1e277a7cda3a7820b9edb
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-05-02 10:32:10 -0700

    Initialize the State Variables and function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 	/*----- constants -----*/
     const PLAYERS = {
-		'0': 'white',
+		'0': 'empty',
 		'1':'X-player',
 	   '-1': 'O-player'
 		
@@ -8,6 +8,9 @@
 
 
 	/*----- state variables -----*/
+	let turn;  // this will be 1 or -1
+	let board; // this will be 2d array of 3 aaray and 3 values
+	let winner; //this will set to null,1,-1 or 'T'
 
 
 
@@ -18,3 +21,12 @@
 
 
 	/*----- functions -----*/
+	function init() {
+		turn = 1;
+		board = [
+			[0,0,0], // col 0
+			[0,0,0], // col 1
+			[0,0,0], // col 2
+		// r0 r1 r2
+		]
+	}
