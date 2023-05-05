6b38d85d52e9a70f98fe082c2128eb44b9c25057
Sapnapalkhade <sapnapalkhade@gmail.com>
2023-05-05 15:00:46 -0700

    made some changes

--- app.js
+++ app.js
@@ -1,66 +1,68 @@
-	/*----- constants -----*/
-    const PLAYERS = {
-		'0': 'empty',
-		'1':'X-player',
-	   '-1': 'O-player'
-		
-	  };
+/*----- constants -----*/
+const PLAYERS = {
+'1': 'X',
+'-1': 'O',
+'null': ''
+
+};
+
+const winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+  ];
+
+/*----- app's state (variables) -----*/
+let board,turn,winner;
 
 
-	/*----- state variables -----*/
-	let turn;  // this will be 1 or -1
-	let board; // this will be 2d array of 3 aaray and 3 values
-	let winner; //this will set to null,1,-1 or 'T'
+
+/*----- cached elements  -----*/
+const messageEl = document.querySelector('h2');
+const buttonEl = document.querySelector('button');
+const boardEl = document.getElementById('board');
+const boardEls = document.querySelectorAll('#board > div');
 
 
-
-	/*----- cached elements  -----*/
-
-
-	/*----- event listeners -----*/
+/*----- event listeners -----*/
 
 
-	/*----- functions -----*/
-	init();
+/*----- functions -----*/
+init();
 
-	function init() {
-		turn = 1;
 
-		//// rotate the board array 90 degrees counter-clockwise
-		board = [
-			[0,0,0], // col 0
-			[0,0,0], // col 1
-			[0,0,0], // col 2
-		// r0 r1 r2
-		];
-		winner = null; // null is indiacates that no winner yet
-		render(); 
+// Initialize all state variables, then call render()
+function init() {
+    board = [null, null, null, null, null, null, null, null, null];
+	// OR initialize like this:
+  // board = new Array(9).fill(null);
+	turn = 1;
+	winner = null; // null is indiacates that no winner yet
+	render();
+}
+// this function transfer the state of our application to the DOM
+function render() {
+	renderBoard();
+	renderMessage();	
+
+}
+
+function renderBoard() {
+	
+}
+
+function renderMessage() {
+	if (winner === 'T') {
+		messageEl.innerText = "It's a Tie!!!";
+	} else if (winner) {
+		messageEl.innerText = `${PLAYERS[winner].toUpperCase()} Wins!`;
+	} else {
+		messageEl.innerText = `${PLAYERS[turn].toUpperCase()} turn`;
 	}
-    // this function transfer the state of our application to the DOM
-	function render() {
-		renderBoard();
-		renderMessage();
-		
-	}
+}
 
-	function renderBoard() {
-		board.forEach(function(colArr,colIdx) {
-			colArr.forEach(function(rowVal, rowIdx) {
-				const cellId = `c${colIdx}r${rowIdx}`;
-				const cellEl = document.getElementById(cellId);
-				console.log(cellEl);
-
-			});
-		});
-
-	}
-
-	function renderMessage() {
-		if (winner === 'T') {
-		  messageEl.innerText = "It's a Tie!!!";
-		} else if (winner) {
-		  messageEl.innerText = `${PLAYERS[winner].toUpperCase()} Wins!`;
-		} else {
-		  messageEl.innerText = `${PLAYERS[turn].toUpperCase()} turn`;
-		}
-	  }
